7b4759814c62bd0f8b4e831bec32f357b54e34ea
ArataUzumaki <mateoemmanuelwalter10@gmail.com>
2024-11-15 13:17:21 +0800

    Updated (11/15/2024-1:17pm)

--- Login/roomoccupancy.Designer.vb
+++ Login/roomoccupancy.Designer.vb
@@ -22,9 +22,168 @@ Partial Class roomoccupancy
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txtroom = New System.Windows.Forms.TextBox()
+        Me.btnconfirm = New System.Windows.Forms.Button()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnregister = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnback = New System.Windows.Forms.Button()
+        Me.cboChoose = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'txtroom
+        '
+        Me.txtroom.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtroom.Location = New System.Drawing.Point(179, 177)
+        Me.txtroom.Name = "txtroom"
+        Me.txtroom.Size = New System.Drawing.Size(199, 29)
+        Me.txtroom.TabIndex = 10
+        '
+        'btnconfirm
+        '
+        Me.btnconfirm.BackColor = System.Drawing.Color.Transparent
+        Me.btnconfirm.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnconfirm.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnconfirm.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.btnconfirm.Location = New System.Drawing.Point(384, 177)
+        Me.btnconfirm.Name = "btnconfirm"
+        Me.btnconfirm.Size = New System.Drawing.Size(123, 32)
+        Me.btnconfirm.TabIndex = 16
+        Me.btnconfirm.Text = "Confirm"
+        Me.btnconfirm.UseVisualStyleBackColor = False
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpassword.Location = New System.Drawing.Point(179, 142)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(199, 29)
+        Me.txtpassword.TabIndex = 91
+        '
+        'txtID
+        '
+        Me.txtID.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtID.Location = New System.Drawing.Point(179, 107)
+        Me.txtID.Name = "txtID"
+        Me.txtID.ReadOnly = True
+        Me.txtID.Size = New System.Drawing.Size(199, 29)
+        Me.txtID.TabIndex = 90
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.Label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label4.Location = New System.Drawing.Point(71, 107)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 24)
+        Me.Label4.TabIndex = 89
+        Me.Label4.Text = "Account ID:"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label2.Location = New System.Drawing.Point(76, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 24)
+        Me.Label2.TabIndex = 88
+        Me.Label2.Text = "Password:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'btnregister
+        '
+        Me.btnregister.BackColor = System.Drawing.Color.Transparent
+        Me.btnregister.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnregister.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnregister.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.btnregister.Location = New System.Drawing.Point(212, 212)
+        Me.btnregister.Name = "btnregister"
+        Me.btnregister.Size = New System.Drawing.Size(123, 32)
+        Me.btnregister.TabIndex = 93
+        Me.btnregister.Text = "Register"
+        Me.btnregister.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Transparent
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.btnClear.Location = New System.Drawing.Point(384, 121)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(123, 32)
+        Me.btnClear.TabIndex = 94
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnback
+        '
+        Me.btnback.BackColor = System.Drawing.Color.Transparent
+        Me.btnback.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnback.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnback.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.btnback.Location = New System.Drawing.Point(470, 239)
+        Me.btnback.Name = "btnback"
+        Me.btnback.Size = New System.Drawing.Size(70, 32)
+        Me.btnback.TabIndex = 95
+        Me.btnback.Text = "Back"
+        Me.btnback.UseVisualStyleBackColor = False
+        '
+        'cboChoose
+        '
+        Me.cboChoose.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.cboChoose.FormattingEnabled = True
+        Me.cboChoose.Items.AddRange(New Object() {"Choose:", "Room Code", "Room Name"})
+        Me.cboChoose.Location = New System.Drawing.Point(52, 178)
+        Me.cboChoose.Name = "cboChoose"
+        Me.cboChoose.Size = New System.Drawing.Size(121, 32)
+        Me.cboChoose.TabIndex = 96
+        '
+        'roomoccupancy
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = Global.Login.My.Resources.Resources.FB_IMG_1729211691620
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(543, 297)
+        Me.Controls.Add(Me.cboChoose)
+        Me.Controls.Add(Me.btnback)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnregister)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnconfirm)
+        Me.Controls.Add(Me.txtroom)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "roomoccupancy"
         Me.Text = "roomoccupancy"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents btnconfirm As Button
+    Friend WithEvents txtpassword As TextBox
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnregister As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnback As Button
+    Friend WithEvents cboChoose As ComboBox
+    Friend WithEvents txtroom As TextBox
 End Class

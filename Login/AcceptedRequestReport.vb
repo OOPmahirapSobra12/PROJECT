f4d01f1ca016d520f4c6038a705b6b67c8710c5f
ArataUzumaki <mateoemmanuelwalter10@gmail.com>
2024-12-06 20:41:03 +0800

    UPdated na ok na siguro

--- Login/AcceptedRequestReport.vb
+++ Login/AcceptedRequestReport.vb
@@ -18,7 +18,7 @@ Imports System
 Imports System.ComponentModel
 
 
-Public Class Roomlist
+Public Class AcceptedRequestReport
     Inherits ReportClass
     
     Public Sub New()
@@ -27,7 +27,7 @@ Public Class Roomlist
     
     Public Overrides Property ResourceName() As String
         Get
-            Return "Roomlist.rpt"
+            Return "AcceptedRequestReport.rpt"
         End Get
         Set
             'Do nothing
@@ -45,7 +45,7 @@ Public Class Roomlist
     
     Public Overrides Property FullResourceName() As String
         Get
-            Return "Login.Roomlist.rpt"
+            Return "Login.AcceptedRequestReport.rpt"
         End Get
         Set
             'Do nothing
@@ -70,7 +70,7 @@ Public Class Roomlist
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property GroupHeaderSection1() As CrystalDecisions.CrystalReports.Engine.Section
+    Public ReadOnly Property Section3() As CrystalDecisions.CrystalReports.Engine.Section
         Get
             Return Me.ReportDefinition.Sections(2)
         End Get
@@ -78,7 +78,7 @@ Public Class Roomlist
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Section3() As CrystalDecisions.CrystalReports.Engine.Section
+    Public ReadOnly Property Section4() As CrystalDecisions.CrystalReports.Engine.Section
         Get
             Return Me.ReportDefinition.Sections(3)
         End Get
@@ -86,31 +86,15 @@ Public Class Roomlist
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property GroupFooterSection1() As CrystalDecisions.CrystalReports.Engine.Section
-        Get
-            Return Me.ReportDefinition.Sections(4)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Section4() As CrystalDecisions.CrystalReports.Engine.Section
-        Get
-            Return Me.ReportDefinition.Sections(5)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
     Public ReadOnly Property Section5() As CrystalDecisions.CrystalReports.Engine.Section
         Get
-            Return Me.ReportDefinition.Sections(6)
+            Return Me.ReportDefinition.Sections(4)
         End Get
     End Property
 End Class
 
 <System.Drawing.ToolboxBitmapAttribute(GetType(CrystalDecisions.[Shared].ExportOptions), "report.bmp")>  _
-Public Class CachedRoomlist
+Public Class CachedAcceptedRequestReport
     Inherits Component
     Implements ICachedReport
     
@@ -152,7 +136,7 @@ Public Class CachedRoomlist
     End Property
     
     Public Overridable Function CreateReport() As CrystalDecisions.CrystalReports.Engine.ReportDocument Implements CrystalDecisions.ReportSource.ICachedReport.CreateReport
-        Dim rpt As Roomlist = New Roomlist()
+        Dim rpt As AcceptedRequestReport = New AcceptedRequestReport()
         rpt.Site = Me.Site
         Return rpt
     End Function
--- Login/AdminPage3.vb
+++ Login/AdminPage3.vb
@@ -82,7 +82,7 @@ Public Class AdminPage3
     End Sub
 
     Private Sub btnnext_Click(sender As Object, e As EventArgs) Handles btnnext.Click
-        AdminPage4.show()
+        'AdminPage4.show()
         Me.Hide()
     End Sub
 End Class
